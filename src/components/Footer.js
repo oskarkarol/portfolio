9362f2f79e3c4f0d910544eb8a889c8b462a0459
oskarkarol <104499175+oskarkarol@users.noreply.github.com>
2022-07-15 12:52:25 +0200

    update domain

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>📧 E-mail: kontakt@oskarkarol.de</h3>
+          <h3>📧 E-mail: kontakt@oskarkarol.xyz</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year}</h3>
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,7 +6,7 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "kontakt@oskarkarol.de",
+          "kontakt@oskarkarol.xyz",
           "LAMP Stack Developer",
           "Open Source Contributor",
         ],
